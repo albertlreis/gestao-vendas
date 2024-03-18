41e8715cf2a7f9663442b3b1ca8dfa34cbbf0978
Albert Reis <albert.reis@4yousee.com.br>
2024-03-18 04:08:24 -0300

    Ajustes conexão com MySQL

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
   dialect: 'mysql',
+  port: 3367
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conectado ao banco "+process.env.MYSQL_DATABASE)
+  }).catch((error) => {
+  console.log("Erro ao conectar com "+process.env.MYSQL_DATABASE, error)
+})
 
 module.exports = sequelize;
