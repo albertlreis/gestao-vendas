fbe83485b18737133998b991551007010a49f3a4
Albert Reis <albert.reis@4yousee.com.br>
2024-03-18 04:06:15 -0300

    Tratamento de erro global

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,12 @@ app.use('/api', routes);
 
 const PORT = process.env.PORT || 3001;
 
+// Tratamento de erro global
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo deu errado!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
